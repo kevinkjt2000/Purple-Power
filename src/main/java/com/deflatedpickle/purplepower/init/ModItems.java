40db5ac3be7dbaedcc7a3467d0ea0b2d6cc070fd
DeflatedPickle <DeflatedPickle@gmail.com>
2017-12-07 19:56:44 +0000

    Added some tool materials

---
+++ src/main/java/com/deflatedpickle/purplepower/init/ModItems.java
@@ -0,0 +1,15 @@
+package com.deflatedpickle.purplepower.init;
+
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
+
+public class ModItems {
+    public static ToolMaterial RUBY = EnumHelper.addToolMaterial("RUBY", 2,750, 6.0f, 2.0f, 18);
+    public static ToolMaterial SAPPHIRE = EnumHelper.addToolMaterial("SAPPHIRE", 2,750, 6.0f, 2.0f, 18);
+    public static ToolMaterial EMERALD = EnumHelper.addToolMaterial("EMERALD", 2,750, 6.0f, 2.0f, 18);
+    public static ToolMaterial AMETHYST = EnumHelper.addToolMaterial("AMETHYST", 2,750, 6.0f, 2.0f, 18);
+
+    public static void init() {
+    }
+}
