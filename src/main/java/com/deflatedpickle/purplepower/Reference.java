acc1a5bba5e4cb9ef61b7039583e94dd58bcc48f
DeflatedPickle <DeflatedPickle@gmail.com>
2017-12-07 20:38:31 +0000

    Made everything public

--- src/main/java/com/deflatedpickle/purplepower/Reference.java
+++ src/main/java/com/deflatedpickle/purplepower/Reference.java
@@ -1,12 +1,12 @@
 package com.deflatedpickle.purplepower;
 
-class Reference {
-    static final String MOD_ID = "purplepower";
-    static final String NAME = "PurplePower";
+public class Reference {
+    public static final String MOD_ID = "purplepower";
+    public static final String NAME = "PurplePower";
     // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
-    static final String VERSION = "1.12.2-0.0.0.0";
-    static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String VERSION = "1.12.2-0.0.0.0";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 
-    static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.purplepower.proxy.ClientProxy";
-    static final String SERVER_PROXY_CLASS = "com.deflatedpickle.purplepower.proxy.ServerProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.purplepower.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "com.deflatedpickle.purplepower.proxy.ServerProxy";
 }
