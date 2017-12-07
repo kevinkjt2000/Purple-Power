ace82a2e60844fa77196cd6e1c576a70459e5fa4
DeflatedPickle <DeflatedPickle@gmail.com>
2017-12-07 18:56:54 +0000

    Added the proxy files

---
+++ src/main/java/com/deflatedpickle/purplepower/proxy/ClientProxy.java
@@ -0,0 +1,7 @@
+package com.deflatedpickle.purplepower.proxy;
+
+public class ClientProxy implements CommonProxy{
+    @Override
+    public void init() {
+    }
+}
---
+++ src/main/java/com/deflatedpickle/purplepower/proxy/CommonProxy.java
@@ -0,0 +1,5 @@
+package com.deflatedpickle.purplepower.proxy;
+
+public interface CommonProxy {
+    void init();
+}
