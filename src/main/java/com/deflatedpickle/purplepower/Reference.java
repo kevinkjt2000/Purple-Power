4fc5774dc3ce2aa1c1e06c692f5bf57e9fda351e
DeflatedPickle <DeflatedPickle@gmail.com>
2017-12-07 19:02:08 +0000

    Added a reference class

---
+++ src/main/java/com/deflatedpickle/purplepower/Reference.java
@@ -0,0 +1,12 @@
+package com.deflatedpickle.purplepower;
+
+class Reference {
+    static final String MOD_ID = "purplepower";
+    static final String NAME = "PurplePower";
+    // Versions follow this format: MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH.
+    static final String VERSION = "1.12.2-0.0.0.0";
+    static final String ACCEPTED_VERSIONS = "[1.12.2]";
+
+    static final String CLIENT_PROXY_CLASS = "com.deflatedpickle.purplepower.proxy.ClientProxy";
+    static final String SERVER_PROXY_CLASS = "com.deflatedpickle.purplepower.proxy.ServerProxy";
+}
