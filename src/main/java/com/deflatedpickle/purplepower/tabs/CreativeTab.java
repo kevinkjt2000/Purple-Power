d26e5bb3a7bf85805c345d5c147892d150929031
DeflatedPickle <DeflatedPickle@gmail.com>
2017-12-07 21:45:08 +0000

    Added a creative tab class

---
+++ src/main/java/com/deflatedpickle/purplepower/tabs/CreativeTab.java
@@ -0,0 +1,21 @@
+package com.deflatedpickle.purplepower.tabs;
+
+import com.deflatedpickle.purplepower.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+
+public class CreativeTab extends CreativeTabs {
+    private String name;
+    private ItemStack icon;
+
+    public CreativeTab(String name, ItemStack icon) {
+        super("tab_" + Reference.MOD_ID + "_" + name);
+        this.name = name;
+        this.icon = icon;
+    }
+
+    @Override
+    public ItemStack getTabIconItem(){
+        return icon;
+    }
+}
