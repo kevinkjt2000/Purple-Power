a7c7f82cde9c8c73e3719ee459930df892f29221
DeflatedPickle <DeflatedPickle@gmail.com>
2017-12-08 02:01:43 +0000

    Changed to uppercase

--- src/main/java/com/deflatedpickle/purplepower/init/ModItems.java
+++ src/main/java/com/deflatedpickle/purplepower/init/ModItems.java
@@ -11,9 +11,9 @@ public class ModItems {
     public static ToolMaterial EMERALD = EnumHelper.addToolMaterial("EMERALD", 2,750, 6.0f, 2.0f, 18);
     public static ToolMaterial AMETHYST = EnumHelper.addToolMaterial("AMETHYST", 2,750, 6.0f, 2.0f, 18);
 
-    public static Item ruby_sword;
+    public static Item RUBY_SWORD;
 
     public static void init() {
-        ruby_sword = new ItemSword("ruby_sword", RUBY);
+        RUBY_SWORD = new ItemSword("ruby_sword", RUBY);
     }
 }
