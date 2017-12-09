8081d310f62762def701a0f57a1af48e0ca7dc89
DeflatedPickle <DeflatedPickle@gmail.com>
2017-12-09 14:48:26 +0000

    Added a tools tab

--- src/main/java/com/deflatedpickle/purplepower/init/ModCreativeTabs.java
+++ src/main/java/com/deflatedpickle/purplepower/init/ModCreativeTabs.java
@@ -6,8 +6,10 @@ import net.minecraft.item.ItemStack;
 
 public class ModCreativeTabs {
     public static CreativeTab COMBAT;
+    public static CreativeTab TOOLS;
 
     public static void init() {
         COMBAT = new CreativeTab("combat", new ItemStack(Items.DIAMOND_SWORD));
+        TOOLS = new CreativeTab("tools", new ItemStack(Items.DIAMOND_SHOVEL));
     }
 }
